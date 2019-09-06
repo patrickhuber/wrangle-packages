93f83d14774ddd39e1619279b449b7fc0365b3ed
patrickhuber <patrickhuber@users.noreply.github.com>
2019-09-06 03:24:51 -0400

    exports path instead of moving file

--- scripts/update-packages.sh
+++ scripts/update-packages.sh
@@ -5,7 +5,7 @@ apt-get update
 apt-get install wget jq -y
 wget 'https://github.com/mikefarah/yq/releases/download/2.4.0/yq_linux_amd64' -O ~/yq
 chmod +x ~/yq
-mv ~/yq /usr/local/bin/yq
+export PATH=$PATH:~/
 
 # enumerate packages looking for resource.yml and template.yml files
 for package in feed; do
