02e536da89003801d3c3715c2cf5af1ae7db4dd7
patrickhuber <patrickhuber@users.noreply.github.com>
2021-02-12 13:22:55 -0500

    updates script to handle egrep version and use github tags

--- scripts/update-packages.sh
+++ scripts/update-packages.sh
@@ -14,31 +14,52 @@ do
   fi
 
   # check the type of the resource, skip if not github-release
-  resource_type=$(yq e $resource_file resource.type)
-  if [ $resource_type != "github-release" ]; then
+  resource_type=$(yq e '.resource.type' $resource_file)
+  if [ "$resource_type" != "github-release" ]; then
     echo "skip: $package, unknown release type '$resource_type'"
     continue;
   fi
   
-  # get latest version from the owner/repository
-  github_release_owner=$(yq e '.resource.source.repository' $resource_file)
-  github_release_repository=$(yq e '.resource.source.repository' $resource_file)
+  # get latest version from the owner/repository  
+  github_release_owner=$(yq e '.resource.source.owner' $resource_file)
+  if [ "$github_release_owner" == "null" ]; then
+    echo "skip: $package, missing github-release.source.owner"
+    continue;
+  fi
 
-  github_release_latest=$(curl -s https://api.github.com/repos/$github_release_owner/$github_release_repository/releases/latest | jq .name -r)
-  github_release_latest=$(echo ${github_release_latest//v})
-  # check for the state file
- 
+  github_release_repository=$(yq e '.resource.source.repository' $resource_file)
+  if [ "$github_release_repository" == "null" ]; then
+    echo "skip: $package, missing github-release.source.repository"
+    continue;
+  fi
+
+  # extract the version regex
+  github_release_version_regex=$(yq e '.resource.source.version-regex' $resource_file)
+  if [ "$github_release_version_regex" == "null" ]; then
+    github_release_version_regex=".*"
+  fi
+
+  # get the latest version and use the version regex to filter (the meaning of this is meant to allow pinning, so rethinking may be necessary)
+  github_release_latest=$(curl -s https://api.github.com/repos/$github_release_owner/$github_release_repository/releases/latest | jq .tag_name -r | grep -E -o "${github_release_version_regex}")
+  if [ "$github_release_latest" == "" ]; then
+    echo "skip: $package, github release version unable to be parsed with expression '$github_release_version_regex'"
+    continue;
+  fi
+    
+  # check for the state file 
   state_file="$package_dir/state.yml"            
 
-  if [ -f $state_file ]; then
+  if [ -f "$state_file" ]; then
     version=$(yq e '.version' $state_file)
-    if [ $version == $github_release_latest ]; then
+    if [ "$version" == "$github_release_latest" ]; then
         echo "skip: $package, on latest version"
         continue;
     fi
+  else
+    echo "version: $github_release_latest" > $state_file
   fi
 
-  version=$(echo ${github_release_latest})
+  export version=$(echo ${github_release_latest})
   package_version_dir="$package_dir/$version"
 
   # write the latest version out to the latest file
